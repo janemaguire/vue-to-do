269bd74fbeed4ebae5df74ad939fae24367f9237
Jane <jane.m.maguire@gmail.com>
2018-05-31 11:41:26 +0100

    Added components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ToDoList />
+    <DoneList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoList from './components/ToDoList.vue'
+import DoneList from './components/DoneList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ToDoList,
+    DoneList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>To Do</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
